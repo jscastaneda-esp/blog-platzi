d1e7603ab2fc0f41166e43cb72c5b2d2d0c07f02
Jonathan Castañeda <jcaatanedaesp@gmail.com>
2022-11-01 18:15:11 -0500

    Uso de Actix Web

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,58 @@
-#[macro_use]
-extern crate diesel;
-
 mod models;
 mod schema;
 
-use dotenvy::dotenv;
-use std::env;
-
+use actix_web::error::ErrorInternalServerError;
+use actix_web::{get, post, web, App, HttpServer, Responder, Result};
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager, Pool};
+use dotenvy::dotenv;
+use models::{NewPostHandler, Post};
+use schema::posts::dsl::posts;
+use std::env;
 
-fn main() {
+pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+#[get("/")]
+async fn index(pool: web::Data<DbPool>) -> Result<impl Responder> {
+    let mut conn = pool.get().expect("Error getting connection from database");
+
+    match web::block(move || posts.load::<Post>(&mut conn).unwrap()).await {
+        Ok(data) => Ok(web::Json(data)),
+        Err(_) => Err(ErrorInternalServerError("Error")),
+    }
+}
+
+#[post("/new_post")]
+async fn new_post(
+    pool: web::Data<DbPool>,
+    item: web::Json<NewPostHandler>,
+) -> Result<impl Responder> {
+    let mut conn = pool.get().expect("Error getting connection from database");
+
+    match web::block(move || Post::create_post(&mut conn, &item).unwrap()).await {
+        Ok(data) => Ok(web::Json(data)),
+        Err(_) => Err(ErrorInternalServerError("Error")),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DB Url config not found");
-    let mut conn =
-        PgConnection::establish(&database_url).expect("Error establish connection with database");
+    let connection = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = Pool::builder()
+        .build(connection)
+        .expect("Error get pool connections database");
 
-    use models::{NewPost, Post};
-    use schema::posts::dsl::{body, id, posts, title};
-
-    println!("INSERT INTO");
-    let new_post = NewPost {
-        title: "Nuevo",
-        body: "LoreExcepteur irure reprehenderit veniam veniam eu Lorem elit minim in ea laborum cillum sint.",
-        slug: "Ut nulla proident cupidatat aliquip voluptate Lorem officia. Mollit ullamco officia magna et irure pariatur Lorem eiusmod reprehenderit. Deserunt amet quis aliquip qui consectetur veniam ex. Sunt dolore nulla consectetur nulla magna. Officia esse nostrud officia deserunt aute cillum voluptate sint Lorem."
-    };
-    diesel::insert_into(posts)
-        .values(&new_post)
-        .get_result::<Post>(&mut conn)
-        .expect("Error insertion record");
-
-    println!("\nSELECT * FROM posts");
-    let posts_result = posts.load::<Post>(&mut conn).expect("Error execute query");
-    for post in posts_result {
-        println!("- {:?}", post);
-    }
-
-    println!("\nSELECT * FROM posts LIMIT 1");
-    let posts_result = posts
-        .limit(1)
-        .load::<Post>(&mut conn)
-        .expect("Error execute query");
-    for post in posts_result {
-        println!("- {:?}", post);
-    }
-
-    println!("\nSELECT title, body FROM posts");
-    let posts_result = posts
-        .select((title, body))
-        .load::<(String, String)>(&mut conn)
-        .expect("Error execute query");
-    for post in posts_result {
-        println!("- {:?}", post);
-    }
-
-    println!("\nSELECT * FROM posts ORDER BY id DESC");
-    let posts_result = posts
-        .order(id.desc())
-        .load::<Post>(&mut conn)
-        .expect("Error execute query");
-    for post in posts_result {
-        println!("- {:?}", post);
-    }
-
-    println!("\nSELECT * FROM posts WHERE id = ?");
-    let posts_result = posts
-        .filter(id.eq(2))
-        .load::<Post>(&mut conn)
-        .expect("Error execute query");
-    for post in posts_result {
-        println!("- {:?}", post);
-    }
-
-    println!("\nUPDATE posts SET title = ? WHERE title = ?");
-    diesel::update(posts.filter(title.eq("Nuevo")))
-        .set(title.eq("My three post"))
-        .get_result::<Post>(&mut conn)
-        .expect("Error updated record");
-
-    println!("\nSELECT * FROM posts WHERE title = ?");
-    let posts_result = posts
-        .filter(title.eq("My three post"))
-        .load::<Post>(&mut conn)
-        .expect("Error execute query");
-    for post in posts_result {
-        println!("- {:?}", post);
-    }
-
-    println!("\nDELETE FROM posts WHERE title = ?");
-    diesel::delete(posts.filter(title.eq("My three post")))
-        .execute(&mut conn)
-        .expect("Error delete record");
-
-    println!("\nSELECT * FROM posts WHERE title = ?");
-    let posts_result = posts
-        .filter(title.eq("My three post"))
-        .load::<Post>(&mut conn)
-        .expect("Error execute query");
-    for post in posts_result {
-        println!("- {:?}", post);
-    }
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(new_post)
+            .app_data(web::Data::new(pool.clone()))
+    })
+    .bind(("127.0.0.1", 9900))?
+    .run()
+    .await
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,8 @@
-#[derive(Queryable, Debug)]
+use diesel::prelude::*;
+use diesel::PgConnection;
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Debug, Deserialize, Serialize)]
 pub struct Post {
     pub id: i32,
     pub title: String,
@@ -6,9 +10,38 @@ pub struct Post {
     pub body: String,
 }
 
+impl Post {
+    pub fn slugify(title: &String) -> String {
+        title.replace(" ", "to").to_lowercase()
+    }
+
+    pub fn create_post<'a>(
+        conn: &mut PgConnection,
+        post: &NewPostHandler,
+    ) -> Result<Post, diesel::result::Error> {
+        let slug = Post::slugify(&post.title);
+
+        let new_post = NewPost {
+            title: &post.title,
+            body: &post.body,
+            slug: &slug,
+        };
+
+        diesel::insert_into(posts::table)
+            .values(new_post)
+            .get_result::<Post>(conn)
+    }
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct NewPostHandler {
+    pub title: String,
+    pub body: String,
+}
+
 use super::schema::posts;
 
-#[derive(Insertable)]
+#[derive(Insertable, Deserialize, Serialize)]
 #[diesel(table_name = posts)]
 pub struct NewPost<'a> {
     pub title: &'a str,
